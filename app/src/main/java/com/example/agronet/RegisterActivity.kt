172cb10e2bf764ad1f8c48f91655265ca99c8f17
Panagiotis Leos <97064473+PanagiotisLeos@users.noreply.github.com>
2024-05-02 19:56:37 +0300

    farmers page created

--- app/src/main/java/com/example/agronet/RegisterActivity.kt
+++ app/src/main/java/com/example/agronet/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.example.agronet
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +11,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+    }
+
+    fun farmersClicked(view: View){
+        setContentView(R.layout.activity_scrolling)
     }
 }
