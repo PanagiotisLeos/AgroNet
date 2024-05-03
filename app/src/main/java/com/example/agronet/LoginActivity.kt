c0e27077316ede9a524433c20ac94d520da146bd
Panagiotis Leos <97064473+PanagiotisLeos@users.noreply.github.com>
2024-05-03 21:45:35 +0300

    hint corrected

--- app/src/main/java/com/example/agronet/LoginActivity.kt
+++ app/src/main/java/com/example/agronet/LoginActivity.kt
@@ -13,8 +13,12 @@ class LoginActivity : AppCompatActivity(){
         setContentView(R.layout.activity_login)
 
         val emailInput = findViewById<EditText>(R.id.email_input)
-
+        val passwordInput = findViewById<EditText>(R.id.password_input)
         val regButton = findViewById<Button>(R.id.register_button)
+
+        setClearHintOnFocus(emailInput)
+        setClearHintOnFocus(passwordInput)
+
         loginClicked(emailInput)
         regButton.setOnClickListener{
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
@@ -26,6 +30,20 @@ class LoginActivity : AppCompatActivity(){
         Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
     }
 
+    private fun setClearHintOnFocus(editText: EditText) {
+        editText.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                editText.hint = ""
+            } else {
+                // Restore the hint when the EditText loses focus
+                if (editText.id == R.id.email_input) {
+                    editText.hint = getString(R.string.email_string)
+                } else if (editText.id == R.id.password_input) {
+                    editText.hint = getString(R.string.password_string)
+                }
+            }
+        }
+    }
 
 
 }
