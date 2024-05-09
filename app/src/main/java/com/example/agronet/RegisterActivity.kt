83ff1feb641269d2ac0e8ac54672d19a2d10934a
Panagiotis Leos <97064473+PanagiotisLeos@users.noreply.github.com>
2024-05-09 17:14:51 +0300

    update

--- app/src/main/java/com/example/agronet/RegisterActivity.kt
+++ app/src/main/java/com/example/agronet/RegisterActivity.kt
@@ -12,9 +12,6 @@ import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 
-import android.os.Bundle
-import android.view.View
-
 import androidx.appcompat.app.AppCompatActivity
 
 
